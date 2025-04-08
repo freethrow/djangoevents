ed4c04aaa8e93b82f00558f70623e830958e5b97
unknown <aleksendric@gmail.com>
2025-04-08 21:07:34 +0200

    settings Postgres

--- eventi/settings.py
+++ eventi/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -89,7 +90,7 @@ DATABASES = {
 
  """
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))),
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
 
